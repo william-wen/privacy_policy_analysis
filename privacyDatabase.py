afc132bbfa020e86b2a516741031cb6eb36b0383
William Wen <william.wen360@gmail.com>
2019-09-07 22:22:02 -0400

    Finished Linear Regression and Added Pandas Library

--- privacyDatabase.py
+++ privacyDatabase.py
@@ -23,9 +23,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # Classification
 from sklearn import svm 
 # Regression
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn import tree
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import mean_squared_error, r2_score
 
 import matplotlib.pyplot as plt
 
@@ -54,9 +56,6 @@ def process_policy(policy, lower_case = True, stem = True, stop_words = True):
         stemmed_policy = " ".join([stemmer.stem(word) for word in policy])
     return stemmed_policy
 
-nltk.download('punkt')
-nltk.download('stopwords')
-
 policies = pd.read_excel('hello.xlsx')
 combine = [policies]
 
@@ -65,23 +64,32 @@ policies['Policy'] = policies['Policy'].apply(process_policy)
 
 # Train Test Split
 X_train, X_test, y_train, y_test = train_test_split(policies['Policy'], policies['Score'], test_size = 0.1, random_state = 1)
-
+m = y_test.size
 # TF-IDF and Vectorization
 tfidf = TfidfVectorizer()
 X_train = tfidf.fit_transform(X_train)
+X_test = tfidf.transform(X_test)
 
 # Training SVM classifier
-svm = svm.SVC(C=1000)
-svm.fit(X_train, y_train)
+# svm = svm.SVC(C=1000, gamma='auto')
+# svm.fit(X_train, y_train)
 
 # Training Polynomial Regression Classifier
-poly = PolynomialFeatures(degree = 2)
-X_poly = poly.fit_transform(X_train)
-y_poly = poly.fit_transform([y_train])
+# poly = PolynomialFeatures(degree = 2)
+# X_train = poly.fit_transform(X_train)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = np.sum((y_pred - y_test)**2)
+rmse = np.sqrt(mse/m)
+# print(rmse)
 
 # Test against Test Set 
-X_test = tfidf.transform(X_test)
-y_pred = svm.predict(X_test)
+# X_test = tfidf.transform(X_test)
+# y_pred = svm.predict(X_test)
 # print(y_pred)
 # print(confusion_matrix(y_test, y_pred))
 
